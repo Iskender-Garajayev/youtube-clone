73ce69eaf4249e25e2ba91a8664a9e3c9c7043e3
Iskender Garajayev <yoloten.isa@gmail.com>
2025-02-14 22:08:20 +0500

    update

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { fetchFromAPI } from '../Util/fetchFromAPI';
 
 const Feed = () => {
+  useEffect(() => {
+    fetchFromAPI('search');
+  }, []);
+
   return (
     <div>
       Feed
